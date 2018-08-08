fa695348b592c09e13ba73f04fa9dcef5d9f5dd0
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-08 15:36:46 +0200

    🚑 get latest Gatsby v2 and drop react-router

--- client/gatsby-node.js
+++ client/gatsby-node.js
@@ -2,7 +2,7 @@ exports.onCreatePage = async ({ page, actions }) => {
   const { createPage } = actions
 
   if (page.path.match(/^\/app/)) {
-    page.matchPath = `/app/:path`
+    page.matchPath = '/app/*'
 
     createPage(page)
   }
