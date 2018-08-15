800ca0b64ca641479720645bc1677783577e7ec4
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-15 13:47:00 +0200

    💄 show app as paper on desktop

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,28 +1,50 @@
 import React from 'react'
+import propTypes from 'prop-types'
 import { Router } from '@reach/router'
 
-import ErrorBoundary from '../components/error-boundary'
+import { withStyles } from '@material-ui/core/styles'
 
+import ErrorBoundary from '../components/error-boundary'
+import { BackendConnection } from '../components/session'
+
+import AppLayout from './app-layout'
 import ClickerList from './clicker-list'
 import Clicker from './clicker'
 import ClickerForm from './clicker-form'
 
-import { BackendConnection } from '../components/session'
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2
+  }
+})
 
-export default class App extends React.Component {
+class App extends React.Component {
+  static propTypes = {
+    classes: propTypes.object.isRequired
+  }
   render () {
+    const { classes } = this.props
+
     return (
       <BackendConnection>
         {({ connection }) => (
           <ErrorBoundary>
-            <Router basepath='/app'>
-              <Clicker path='/clickers/:docId' connection={connection} />
-              <ClickerList path='/' connection={connection} />
-              <ClickerForm path='/add' connection={connection} />
-            </Router>
+            <AppLayout>
+              <div className={classes.root}>
+                <Router basepath='/app'>
+                  <Clicker path='/clickers/:docId' connection={connection} />
+                  <ClickerList path='/' connection={connection} />
+                  <ClickerForm path='/add' connection={connection} />
+                </Router>
+              </div>
+            </AppLayout>
           </ErrorBoundary>
         )}
       </BackendConnection>
     )
   }
 }
+
+export default withStyles(styles)(App)
