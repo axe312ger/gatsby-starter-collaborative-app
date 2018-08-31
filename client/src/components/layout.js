920d75632b037245b2e1619b386d6523ddafbd2a
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-31 18:52:56 +0200

    📱 improve meta tags for sharing
    fixes #37

--- client/src/components/layout.js
+++ client/src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
+import { Location } from '@reach/router'
 
 import { withStyles } from '@material-ui/core/styles'
 
@@ -45,44 +46,78 @@ class Layout extends React.Component {
             site {
               siteMetadata {
                 title
+                description
               }
             }
           }
         `}
         render={(data) => (
-          <SessionProvider>
-            <Helmet
-              htmlAttributes={{
-                lang: 'en'
-              }}
-              title={data.site.siteMetadata.title}
-              link={[
-                {
-                  rel: 'apple-touch-icon',
-                  href: '/apple-touch-icon.png'
-                }
-              ]}
-              meta={[
-                {
-                  name: 'description',
-                  content: data.site.siteMetadata.description
-                },
-                { name: 'apple-mobile-web-app-capable', content: 'yes' },
-                {
-                  name: 'apple-mobile-web-app-status-bar-style',
-                  content: 'black-translucent'
-                },
-                { name: 'format-detection', content: 'telephone=no' }
-              ]}
-            />
-            <div className={classes.wrapper} wrapper='true'>
-              <Header siteTitle={data.site.siteMetadata.title} />
-              <main className={classes.content} content='true'>
-                {children}
-              </main>
-            </div>
-            <Drawer />
-          </SessionProvider>
+          <Location>
+            {({ location }) => (
+              <SessionProvider>
+                <Helmet
+                  /**
+                   * Meta information based on:
+                   * https://moz.com/blog/meta-data-templates-123
+                   * https://developer.apple.com/library/archive/documentation/AppleApplications/Reference/SafariWebContent/ConfiguringWebApplications/ConfiguringWebApplications.html
+                   */
+                  htmlAttributes={{
+                    lang: 'en'
+                  }}
+                  title={data.site.siteMetadata.title}
+                  link={[
+                    {
+                      rel: 'apple-touch-icon',
+                      href: '/apple-touch-icon.png'
+                    }
+                  ]}
+                  meta={[
+                    {
+                      name: 'description',
+                      content: data.site.siteMetadata.description
+                    },
+                    {
+                      name: 'twitter:card',
+                      value: 'summary'
+                    },
+                    {
+                      property: 'og:title',
+                      content: data.site.siteMetadata.title
+                    },
+                    { property: 'og:type', content: 'website' },
+                    {
+                      property: 'og:url',
+                      content: `https://gatsby-starter-collaborative-app.netlify.com${
+                        location.pathname
+                      }`
+                    },
+                    {
+                      property: 'og:image',
+                      content:
+                        'https://gatsby-starter-collaborative-app.netlify.com/opengraph-image.png'
+                    },
+                    {
+                      property: 'og:description',
+                      content: data.site.siteMetadata.description
+                    },
+                    { name: 'apple-mobile-web-app-capable', content: 'yes' },
+                    {
+                      name: 'apple-mobile-web-app-status-bar-style',
+                      content: 'black-translucent'
+                    },
+                    { name: 'format-detection', content: 'telephone=no' }
+                  ]}
+                />
+                <div className={classes.wrapper} wrapper='true'>
+                  <Header siteTitle={data.site.siteMetadata.title} />
+                  <main className={classes.content} content='true'>
+                    {children}
+                  </main>
+                </div>
+                <Drawer />
+              </SessionProvider>
+            )}
+          </Location>
         )}
       />
     )
