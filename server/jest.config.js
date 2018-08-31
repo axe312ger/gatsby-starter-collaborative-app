e1f1e5bc25eb73a3da3a49350e88fecd81347788
Benedikt Rötsch <info@benedikt-roetsch.de>
2018-08-31 15:06:59 +0200

    🏗 code coverage via codecov

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,17 +1,9 @@
 module.exports = {
-  "roots": [
-    "<rootDir>/src"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
+  coverageDirectory: '../coverage/',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
